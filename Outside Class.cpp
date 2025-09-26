33fab5bd56978c47ada0d7e8d8a6d60de8ae9f3b
Anavi06-tech <anvik0105@gmail.com>
2025-09-26 11:09:51 +0530

    Create Outside Class.cpp

---
+++ Outside Class.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    public:
+    void myMethod();
+};
+
+void Student::myMethod()
+{
+    cout<<"I am Aditya";
+}
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+// OUTPUT
+// I am Anavi
